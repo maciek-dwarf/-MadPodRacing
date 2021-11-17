6432ebaf00cd7e699e661e7e53195f26e69318cc
Maciej <m1mostowski@gmail.com>
2021-11-17 20:36:59 +0100

    Added vector2d class, code refactoring

--- madpodracing.cpp
+++ madpodracing.cpp
@@ -25,37 +25,175 @@ namespace {
     constexpr int PLAYER_COLLISION_TRESHOLD = 2100;
 }
 
+
+class Vector2D
+{ 
+public:
+    Vector2D(int xx, int yy)
+    {
+        x = xx;
+        y = yy;
+    }
+    Vector2D(float xx = 0, float yy = 0)
+    {
+        x = xx;
+        y = yy;
+    }
+    ~Vector2D() = default;
+ 
+    void Rotate( const float angle )
+    {
+        float xt = (x * cosf(angle)) - (y * sinf(angle));
+        float yt = (y * cosf(angle)) + (x * sinf(angle));
+        x = xt;
+        y = yt;
+    };
+    float Magnitude() const
+    {
+        return sqrtf(x * x + y * y);
+    };
+
+    float Normalize()
+    {
+        float mag = Magnitude();
+ 
+        if(mag != 0.0f)
+        {
+            x /= mag;
+            y /= mag;
+        }
+ 
+        return mag;
+
+    };
+
+    float DotProduct( const Vector2D& v2 ) const
+    {
+        return (x * v2.x) + (y * v2.y);
+    };
+
+    float CrossProduct( const Vector2D& v2 ) const
+    {
+        return (x * v2.y) - (y * v2.x);
+    };
+ 
+    static Vector2D Zero()
+    {
+        return Vector2D(0, 0);
+    };
+
+    static float Distance( const Vector2D& v1, const Vector2D& v2)
+    {
+        return sqrtf( pow((v2.x - v1.x), 2 ) + pow((v2.y - v1.y), 2) );
+    };
+ 
+    Vector2D& operator= ( const Vector2D& v2 )
+    {
+        if (this == &v2)
+            return *this;
+ 
+        x = v2.x;
+        y = v2.y;
+    
+        return *this;
+    };
+ 
+    Vector2D& operator+= ( const Vector2D& v2 )
+    {        
+        x += v2.x;
+        y += v2.y;
+    
+        return *this;
+    };
+
+    Vector2D& operator-= ( const Vector2D& v2 )
+    {
+        
+        x -= v2.x;
+        y -= v2.y;
+    
+        return *this;
+    };
+
+    Vector2D& operator*= ( const float scalar)
+    {
+        x *= scalar;
+        y *= scalar;
+ 
+        return *this;
+    };
+
+    Vector2D& operator/= ( const float scalar)
+    {
+        x /= scalar;
+        y /= scalar;
+    
+        return *this;
+    };
+ 
+    const Vector2D operator+( const Vector2D &v2 ) const
+    {
+        return Vector2D(*this) += v2;
+    };
+
+    const Vector2D operator-( const Vector2D &v2 ) const
+    {
+        return Vector2D(*this) -= v2;
+    };
+
+    const Vector2D operator*( const float scalar ) const
+    {
+        return Vector2D(*this) *= scalar;
+    };
+
+    const Vector2D operator/( const float scalar ) const
+    {
+        return Vector2D(*this) /= scalar;
+    };
+ 
+    bool operator== ( const Vector2D& v2 ) const
+    {
+        return ((x == v2.x) && (y == v2.y));
+    };
+
+    bool operator!= ( const Vector2D& v2 ) const
+    {
+        return !((x == v2.x) && (y == v2.y));
+    };
+ 
+public:
+    float x, y;
+};
+
+
 class Game
 {
     private:
-        int in_pos_x;
-        int in_pos_y;        
-        int checkpoint_x;
-        int checkpoint_y;
-        int prev_checkpoint_x;
-        int prev_checkpoint_y;
+        Vector2D in_pos;
+        
+        Vector2D checkpoint;
+        
+        Vector2D prev_checkpoint;
+        
         int checkpoint_dist;
         int checkpoint_angle;
-        int opponent_x;
-        int opponent_y;
+        Vector2D opponent_pos;
         float prev_dist;
         float route_dist;
-        int speed_x;
-        int speed_y;
+        Vector2D speed;
         
     public:
     Game()
     {
-        in_pos_x = 0;
-        in_pos_y = 0;
-        checkpoint_x = 0;
-        checkpoint_y = 0;
+        in_pos = Vector2D::Zero();
+        checkpoint = Vector2D::Zero();
+        
         checkpoint_dist = 0;
         checkpoint_angle = 0;
-        opponent_x = 0;
-        opponent_y = 0;
-        prev_checkpoint_x = 1000000000;
-        prev_checkpoint_y = 1000000000;
+        opponent_pos = Vector2D::Zero();
+        
+        prev_checkpoint = Vector2D(1000000000, 1000000000);
+        
         route_dist = std::numeric_limits<float>::max();        
     }
 
@@ -68,33 +206,31 @@ class Game
                 const int& opp_x,
                 const int& opp_y)
     {
-        speed_x = x - in_pos_x;
-        speed_y = y - in_pos_y;
-        in_pos_x = x;
-        in_pos_y = y;
-        bool changepod = false;
-        if(checkpoint_x != next_checkpoint_x || checkpoint_y != next_checkpoint_y)
-        {
-            prev_checkpoint_x = checkpoint_x;
-            prev_checkpoint_y = checkpoint_y;
-            changepod = true;
-        }
-        float offset_x = in_pos_x - prev_checkpoint_x;
-        float offset_y = in_pos_y - prev_checkpoint_y;
-        prev_dist  = std::sqrt((float)(offset_x*offset_x + offset_y*offset_y));
-        if(changepod)
-        {
-            route_dist = next_checkpoint_dist;
-        }
+        speed = Vector2D(x,y) - in_pos;
         
-        checkpoint_x = next_checkpoint_x;
-        checkpoint_y = next_checkpoint_y;
-        checkpoint_x -= 3*speed_x;
-        checkpoint_y -= 3*speed_y;
+        in_pos = Vector2D(x,y);
+        
+       // bool changepod = false;
+        Vector2D next_checkpoint(next_checkpoint_x, next_checkpoint_y);
+        if(checkpoint != next_checkpoint )
+        {
+            prev_checkpoint = checkpoint;
+            route_dist = next_checkpoint_dist;
+            //changepod = true;
+        }
+
+        Vector2D offset = in_pos - prev_checkpoint;
+        
+        prev_dist  = offset.Magnitude();
+        
+        
+        checkpoint = Vector2D(next_checkpoint_x,next_checkpoint_y) ;
+        checkpoint -= speed*3.0f;
+        
         checkpoint_dist = next_checkpoint_dist;
         checkpoint_angle = next_checkpoint_angle;
-        opponent_x = opp_x;
-        opponent_y = opp_y;
+        opponent_pos = Vector2D(opp_x, opp_y);
+        
     }
 
     void ComputeValues(int& out_x, int& out_y, std::string& sthrust, bool& boost)
@@ -109,9 +245,6 @@ class Game
         else{             
             thrust = MAX_THRUST;
         }
-
-        //float thrust_scale = std::abs((float) checkpoint_angle / (float) ANGLE_TRESHOLD);
-        //thrust -= thrust_scale*thrust;
 
         cerr << "distance " << checkpoint_dist << endl;
         cerr << "angle " << checkpoint_angle << endl;
@@ -133,28 +266,29 @@ class Game
             thrust_scale = (float)MIN_THRUST + ((float)(MAX_THRUST-MIN_THRUST)* thrust_scale);
             thrust_scale /= (float)MAX_THRUST;
             thrust *= thrust_scale;
-            //thrust = std::clamp(thrust, MIN_THRUST, MAX_THRUST);
+            
             cerr << "  thrust_scale " << thrust_scale << endl;
             cerr << "  thrust " << thrust << endl;
-        } else if(prev_dist <  PREV_SLOW_DAWN_TRESHOLD && route_dist < LONG_ROUTE_TRESHOLD 
+        }//checking if we should slow dawn after passing checkpoint 
+        else if(prev_dist <  PREV_SLOW_DAWN_TRESHOLD && route_dist < LONG_ROUTE_TRESHOLD 
         && std::abs(checkpoint_angle) > 90)
         {
             float thrust_scale = std::abs((float)prev_dist / (float) PREV_SLOW_DAWN_TRESHOLD);            
             thrust_scale = std::clamp(thrust_scale, 0.0f, 1.0f);
+            thrust_scale = (float)MIN_THRUST + ((float)(MAX_THRUST-MIN_THRUST)* thrust_scale);
+            thrust_scale /= (float)MAX_THRUST;
             
             thrust *= thrust_scale;            
             thrust = std::clamp(thrust, MIN_PTHRUST, MAX_THRUST);
             cerr << "  thrust prev dist " << thrust << endl;
         }
-        //float offset_x = in_pos_x - prev_checkpoint_x;
-        float offset_pl_opp_x =  in_pos_x - opponent_x;
-        float offset_pl_opp_y =  in_pos_y - opponent_y;
-        float opponent_player_dist = std::sqrt(offset_pl_opp_x*offset_pl_opp_x +
-         offset_pl_opp_y*offset_pl_opp_y);
-        float offset_pod_opp_x =  checkpoint_x - opponent_x;
-        float offset_pod_opp_y =  checkpoint_y - opponent_y; 
-        float opponent_pod_dist = std::sqrt(offset_pod_opp_x*offset_pod_opp_x +
-         offset_pod_opp_y*offset_pod_opp_y);
+        
+        Vector2D offset_pl_opp =  in_pos - opponent_pos;
+        
+        float opponent_player_dist = offset_pl_opp.Magnitude();
+        Vector2D offset_pod_opp =  checkpoint - opponent_pos;
+        
+        float opponent_pod_dist = offset_pod_opp.Magnitude();
         cerr << " opponent_player_dist " <<  opponent_player_dist<< endl;
         if(PLAYER_COLLISION_TRESHOLD > opponent_player_dist && std::abs(checkpoint_angle) < 90 )
         {
@@ -180,8 +314,8 @@ class Game
             sthrust = std::to_string(thrust);
         }
         
-        out_x = checkpoint_x;
-        out_y = checkpoint_y;
+        out_x = checkpoint.x;
+        out_y = checkpoint.y;
         
     }
 
